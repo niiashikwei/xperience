6f5a4da440cb192cd1d0d67e50db629852de2b27
Nii <niiashikwei@hotmail.com>
2016-08-25 16:02:24 -1000

    Nii - firebase authentication working now via FB

--- app/src/main/java/ideaz/xperience/activities/DashBoardActivity.java
+++ app/src/main/java/ideaz/xperience/activities/DashBoardActivity.java
@@ -3,14 +3,18 @@ package ideaz.xperience.activities;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import ideaz.xperience.R;
 import ideaz.xperience.util.CustomToast;
 
-public class DashboardActivity extends Activity{
+public class DashboardActivity extends Activity {
     @Override
     public void onCreate(Bundle bundle){
         super.onCreate(bundle);
         setContentView(R.layout.activity_dashboard);
-        CustomToast.displayMessage(getApplicationContext(), "logged in successfully!");
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        CustomToast.displayMessage(getApplicationContext(), String.format("User is %s", currentUser.getDisplayName()));
     }
 }
--- app/src/main/java/ideaz/xperience/activities/LoginActivity.java
+++ app/src/main/java/ideaz/xperience/activities/LoginActivity.java
@@ -3,6 +3,7 @@ package ideaz.xperience.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -11,27 +12,61 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
 
 import ideaz.xperience.R;
 import ideaz.xperience.util.CustomToast;
 
-public class LoginActivity extends Activity{
+public class LoginActivity extends Activity {
     private CallbackManager callbackManager;
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        AccessToken currentFBAccessToken = AccessToken.getCurrentAccessToken();
-        if(currentFBAccessToken != null && !currentFBAccessToken.isExpired()){
-            CustomToast.displayMessage(getApplicationContext(), "User already logged in!");
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
             startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
-
         } else {
-            registerFBLoginCallbacks();
-            setContentView(R.layout.activity_login);
-            LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-            loginButton.setReadPermissions("email", "public_profile");
+            AccessToken currentFBAccessToken = AccessToken.getCurrentAccessToken();
+            if(currentFBAccessToken != null && !currentFBAccessToken.isExpired()){
+                CustomToast.displayMessage(getApplicationContext(), "User already logged into facebook.");
+                handleFacebookAccessToken(currentFBAccessToken);
+            } else {
+                registerFBLoginCallbacks();
+                setContentView(R.layout.activity_login);
+                LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+                loginButton.setReadPermissions("email", "public_profile");
+            }
         }
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        CustomToast.displayMessage(getApplicationContext(), "handleFacebookAccessToken:" + token);
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        Task<AuthResult> authResultTask = firebaseAuth.signInWithCredential(credential);
+        authResultTask.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        CustomToast.displayMessage(getApplicationContext(), "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            CustomToast.displayMessage(getApplicationContext(), "signInWithCredential" + task.getException());
+                            CustomToast.displayMessage(getApplicationContext(), "Authentication failed.");
+                        } else {
+                            startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                        }
+                    }
+                });
+
     }
 
     @Override
